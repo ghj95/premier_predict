b9e825d3d24aa126805405f565f1f67e56f5ba14
ghj95 <149880159+ghj95@users.noreply.github.com>
2025-03-27 10:53:29 -0400

    xgb

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ import streamlit as st
 from datetime import datetime
 from notebook_functions import predire_matchs, equipes
 
-st.set_page_config(page_title='FootForecast', page_icon='⚽️', layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title='PremierPredict', page_icon='⚽️', layout='wide', initial_sidebar_state='expanded')
 
-st.markdown('## ⚽️ FootForecast : *Prédictions des matchs de la Premier League*')
-gab_url = 'https://ghj95.github.io/portfolio//'
+st.markdown('## ⚽️ PremierPredict : *Prédictions des matchs de la Premier League*')
+url = 'https://ghj95.github.io/portfolio//'
 st.markdown(
-    f"<a href='{gab_url}' target='_blank' style='text-decoration: none; color: inherit;'>`Par : Gabriel Hardy-Joseph, Karim Ghandour, Louis-Matteo Creplet et Mohamed-Aziz Charfeddine`</a>",
+    f"<a href='{url}' target='_blank' style='text-decoration: none; color: inherit;'>`Par : Gabriel Hardy-Joseph, Karim Ghandour, Louis-Matteo Creplet et Mohamed-Aziz Charfeddine`</a>",
     unsafe_allow_html=True,
 )
 st.markdown(
     "<span style='text-decoration: none;'><code style='color: inherit;'>Projet réalisé dans le cadre du cours TECH20704</code></span>",
     unsafe_allow_html=True,
 )
-
 def appinfo():
     with st.container(border=True):
-        st.write("Notre application s'appuie sur un modèle de classification **Extreme Gradient Boosting (XGBoost)** entraîné sur une décennie de résultats historiques (2015-2025). Les prédictions intègrent la *forme récente des équipes*, l'*analyse statistique des matchs précédents* et l'*historique des confrontations directes* entre les clubs.")
+        st.write("Cette application s'appuie sur un modèle de classification **Extreme Gradient Boosting (XGBoost)** entraîné sur 25 ans de résultats historiques (2000-2025). Les prédictions intègrent la *forme récente des équipes*, l'*analyse statistique des matchs précédents* et l'*historique des confrontations directes* entre les clubs. Essayez-la!")
+    with st.container(border=True):
         st.write('Les prédictions du modèle ne sont pas des certitudes.')
 
 st.sidebar.header('Détails du match')
@@ -31,12 +31,15 @@ domicile, exterieur, match_date = input_features()
 
 appinfo()
 '---'
-
+    
 prediction = None
 if st.sidebar.button('Prédire'):
     valid_input = True
     if domicile and exterieur not in equipes:
         st.error('Veuillez séléctionner deux équipes parmi celles dans la liste')
+        valid_input = False
+    if domicile == exterieur:
+        st.error('Veuillez sélectionner deux équipes différentes')
         valid_input = False
     if match_date.year > datetime.today().year + 1:
         st.error('Veuillez sélectionner une date moins éloignée')
@@ -46,9 +49,11 @@ if st.sidebar.button('Prédire'):
         valid_input = False
     if valid_input:
         with st.spinner('Chargement des prédictions...'):
-            prediction = predire_matchs(domicile, exterieur, match_date)
+            prediction, prob = predire_matchs(domicile, exterieur, match_date)
 
-if prediction == 0:
-    st.success(f'Le modèle prédit une victoire de **{exterieur}** !', icon='🎯')
-elif prediction == 1:
-    st.success(f'Le modèle prédit une victoire de **{domicile}** !', icon='🎯')
+    if prediction == 0:
+        st.success(f'Le modèle prédit une victoire de **{exterieur}** à **{prob[0, 0]:.0%}**!', icon='🎯')
+    elif prediction == 1:
+        st.success(f'Le modèle prédit une victoire de **{domicile}** **{prob[0, 1]:.0%}**!', icon='🎯')
+    else:
+        prediction = print(f"Le modèle prédit un match nul à **{prob[0, 2]:.0%}**!")
--- notebook_functions.py
+++ notebook_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import xgboost as xgb
+import pickle
 
-matchs = pd.read_csv('epl_2015-2025.csv')
+matchs = pd.read_csv('epl_2000-2025.csv')
 
 matchs.rename(columns={'HomeTeam':'Domicile',
                         'AwayTeam':'Exterieur',
@@ -11,12 +11,17 @@ matchs.rename(columns={'HomeTeam':'Domicile',
                         'FTAG':'Buts exterieur',
                         }, inplace=True)
 
-for i in range(380, 760):                                  
+for i in range(len(matchs)):                                     
     row = matchs['Date'][i]                                   
-    jour = row[2:4]                                           
-    mois = row[5:7]                                           
-    annee = row[-2:]                                          
-    matchs.loc[i, 'Date'] = f'{jour}/{mois}/20{annee}'  
+    if row[4:5] == '-':
+        jour = row[2:4]                                           
+        mois = row[5:7]                                           
+        annee = row[-2:]                                          
+        matchs.loc[i, 'Date'] = f'{jour}/{mois}/20{annee}'
+    elif row[-3:-2] == '/':
+        date = row[:-3]                                           
+        annee = row[-2:]                                         
+        matchs.loc[i, 'Date'] = f'{date}/20{annee}'      
 
 matchs['Date'] = pd.to_datetime(matchs['Date'], dayfirst=True)
 
@@ -35,12 +40,10 @@ matchs['JoursSaison'] = matchs.apply(lambda x: (x['Date'] - pd.Timestamp(year=x[
 matchs['SemaineSaison'] = matchs['JoursSaison'] // 7 + 1
 
 def stats_equipe(matchs):                      
-    matchs = matchs.sort_values('Date')                 # on trie les données par date
-    
-    matchs['DomicileForme'] = 0                         # on initialise deux colonnes ou seront stockés les points de forme
+    matchs = matchs.sort_values('Date')              
+    matchs['DomicileForme'] = 0                         
     matchs['ExterieurForme'] = 0
 
-    # on initialise les colonnes où seront stockées les statistiques de but
     colonnes_buts = [
     'DomicileAvgButsMarques_Home', 'DomicileAvgButsEncaisses_Home',     
     'DomicileAvgButsMarques_Away', 'DomicileAvgButsEncaisses_Away',
@@ -50,63 +53,57 @@ def stats_equipe(matchs):
     for col in colonnes_buts:
         matchs[col] = 0.0
 
-    for i in range(len(matchs)):                        # on itère à travers les matchs
-        match_eval = matchs.iloc[i]                     # on stocke les information du match évalué dans la variable match_eval
-        equipe_domicile = match_eval['DomicileCode']
-        equipe_exterieur = match_eval['ExterieurCode']
-        date_match = match_eval['Date']
-        saison_match = match_eval['AnneeSaison']
+    
+    for i in range(len(matchs)):                         
+        match_actuel = matchs.iloc[i]                   
+        equipe_domicile = match_actuel['DomicileCode']  
+        equipe_exterieur = match_actuel['ExterieurCode']
+        date_match = match_actuel['Date']               
+        saison_match = match_actuel['AnneeSaison']      
 
-        # pour l'équipe à domicile la fonction trouve tous les matchs antérieurs à la date du match actuel où l'équipe a joué et les trie par date décroissante en consérvant les 5 plus récents
         prec_domicile_5 = matchs[(matchs['Date'] < date_match) &
                                ((matchs['DomicileCode'] == equipe_domicile) | (matchs['ExterieurCode'] == equipe_domicile))]
         prec_domicile_5 = prec_domicile_5.sort_values('Date', ascending=False).head(5)
         
-        # pour l'équipe à domicile la fonction trouve tous les matchs antérieurs depuis le début de la saison où l'équipe a joué et les triepar date décroissante
-        prec_domicile_saison = matchs[(matchs['Date'] < date_match) & (matchs['AnneeSaison'] == saison_match) &
-                               ((matchs['DomicileCode'] == equipe_domicile) | (matchs['ExterieurCode'] == equipe_domicile))]
-        
-        # même processus pour l'équipe à l'extérieur
         prec_exterieur_5 = matchs[(matchs['Date'] < date_match) & 
                                 ((matchs['DomicileCode'] == equipe_exterieur) | (matchs['ExterieurCode'] == equipe_exterieur))]
         prec_exterieur_5 = prec_exterieur_5.sort_values('Date', ascending=False).head(5)
 
+        prec_domicile_saison = matchs[(matchs['Date'] < date_match) & (matchs['AnneeSaison'] == saison_match) &
+                               ((matchs['DomicileCode'] == equipe_domicile) | (matchs['ExterieurCode'] == equipe_domicile))]
+        
         prec_exterieur_saison = matchs[(matchs['Date'] < date_match) & (matchs['AnneeSaison'] == saison_match) & 
                                 ((matchs['DomicileCode'] == equipe_exterieur) | (matchs['ExterieurCode'] == equipe_exterieur))]
 
-        # calcul des points de forme pour l'équipe à domicile
-        # on initialise la variable qui comptera le nombre de points récoltés à domicile
         points_domicile = 0
-        for j in range(len(prec_domicile_5)):             # on itère à travers les matchs précédents
-            match_prec = prec_domicile_5.iloc[j]          # on stocke les informations de chaque itération du match précédant dans la variable match_prec
-            # on itére à travers les 5 derniers matchs et on ajoute 1 point pour chaque victoire à domicile ou à l'extérieur
+        for j in range(len(prec_domicile_5)):             
+            match_prec = prec_domicile_5.iloc[j]          
             if match_prec['DomicileCode'] == equipe_domicile and match_prec['Cible'] == 1:
-                points_domicile += 1
+                points_domicile += 3
             elif match_prec['ExterieurCode'] == equipe_domicile and match_prec['Cible'] == 0:
+                points_domicile += 3
+            else:
                 points_domicile += 1
 
-        # on répète la même procédure pour l'équipe à l'extérieur
         points_exterieur = 0
         for l in range(len(prec_exterieur_5)):          
             match_prec = prec_exterieur_5.iloc[l]          
             if match_prec['DomicileCode'] == equipe_exterieur and match_prec['Cible'] == 1:
-                points_exterieur += 1
+                points_exterieur += 3
             elif match_prec['ExterieurCode'] == equipe_exterieur and match_prec['Cible'] == 0:
-                points_exterieur += 1
-       
-        # on met à jour les colonnes récoltant le total des points à domicile et à l'extérieur
+                points_exterieur += 3
+            else:
+                points_domicile += 1
+    
         matchs.at[i, 'DomicileForme'] = points_domicile
         matchs.at[i, 'ExterieurForme'] = points_exterieur    
 
-        # calcul des statistiques de buts pour l'équipe à domicile
         dom_home_matches = prec_domicile_saison[prec_domicile_saison['DomicileCode'] == equipe_domicile]
         dom_away_matches = prec_domicile_saison[prec_domicile_saison['ExterieurCode'] == equipe_domicile]
         
-        # calcul des statistiques de buts pour l'équipe à l'exterieur
         ext_home_matches = prec_exterieur_saison[prec_exterieur_saison['DomicileCode'] == equipe_exterieur]
         ext_away_matches = prec_exterieur_saison[prec_exterieur_saison['ExterieurCode'] == equipe_exterieur]
 
-        # calcul des moyennes de buts pour l'équipe à domicile
         if len(dom_home_matches) > 0:
             matchs.at[i, 'DomicileAvgButsMarques_Home'] = dom_home_matches['Buts domicile'].mean()
             matchs.at[i, 'DomicileAvgButsEncaisses_Home'] = dom_home_matches['Buts exterieur'].mean()
@@ -115,7 +112,6 @@ def stats_equipe(matchs):
             matchs.at[i, 'DomicileAvgButsMarques_Away'] = dom_away_matches['Buts exterieur'].mean()
             matchs.at[i, 'DomicileAvgButsEncaisses_Away'] = dom_away_matches['Buts domicile'].mean()
         
-        # calculs des moyennes de buts pour l'équipe extérieur  
         if len(ext_home_matches) > 0:
             matchs.at[i, 'ExterieurAvgButsMarques_Home'] = ext_home_matches['Buts domicile'].mean()
             matchs.at[i, 'ExterieurAvgButsEncaisses_Home'] = ext_home_matches['Buts exterieur'].mean()
@@ -124,11 +120,7 @@ def stats_equipe(matchs):
             matchs.at[i, 'ExterieurAvgButsMarques_Away'] = ext_away_matches['Buts exterieur'].mean()
             matchs.at[i, 'ExterieurAvgButsEncaisses_Away'] = ext_away_matches['Buts domicile'].mean()
     
-    # on crée une nouvelle colonne récoltant la différence entre les points de forme à domicile et à l'extérieur
     matchs['DiffForme'] = matchs['DomicileForme'] - matchs['ExterieurForme']
-
-    # on crée de nouvelles colonnes récoltant la différence des performance de buts entre l'équipe à domicile et celle à l'extérieur
-    # on fait cela en calculant le différentiel de buts, représentant la différence entre la moyenne de buts marqués et la moyenne de buts encaissés
     matchs['DiffButsDomicile'] = matchs['DomicileAvgButsMarques_Home'] - matchs['DomicileAvgButsEncaisses_Home']
     matchs['DiffButsExterieur'] = matchs['ExterieurAvgButsMarques_Away'] - matchs['ExterieurAvgButsEncaisses_Away']
     matchs['DiffButs'] = matchs['DiffButsDomicile'] - matchs['DiffButsExterieur']
@@ -139,102 +131,89 @@ def stats_equipe(matchs):
     return matchs
 
 def faf_equipes(matchs):
-    matchs = matchs.sort_values('Date')                 # on trie les données par date
-    matchs['FAF_VictoiresDomicile'] = 0                 # on initialise deux colonnes ou seront stockés les victoires en face-à-face
-    matchs['FAF_VictoiresExterieur'] = 0
+    matchs = matchs.sort_values('Date')                  
+    matchs['FAF_VictoiresDomicile_Dom'] = 0              
+    matchs['FAF_VictoiresDomicile_Ext'] = 0              
+    matchs['FAF_VictoiresExterieur_Dom'] = 0             
+    matchs['FAF_VictoiresExterieur_Ext'] = 0             
+    matchs['FAF_Nuls_Dom'] = 0                           
+    matchs['FAF_Nuls_Ext'] = 0                           
 
-    for i in range(len(matchs)):
-        match_eval = matchs.iloc[i]                     # on stocke les information du match évalué dans la variable match_eval
-        equipe_domicile = match_eval['DomicileCode']
-        equipe_exterieur = match_eval['ExterieurCode']
-        date_match = match_eval['Date']
-
-        # on cherche à travers tous les matchs précédent la date du match évalué pour stocker dans prec_faf les matchs ou les deux équipes ont joué l'une contre l'autre
-        prec_faf = matchs[(matchs['Date'] < date_match) & 
-                    (((matchs['DomicileCode'] == equipe_domicile) & (matchs['ExterieurCode'] == equipe_exterieur)) | 
-                    ((matchs['DomicileCode'] == equipe_exterieur) & (matchs['ExterieurCode'] == equipe_domicile)))]
-
-        # calcul du nombre de victoires et de matchs nuls lors des face-à-face
-        # on initialise les variables qui vont compter les victoires lors des faces-à-faces
-        victoires_domicile = 0
-        victoires_exterieur = 0
+    for i in range(len(matchs)):                         
         
-        for j in range(len(prec_faf)):                  # on itère à travers les matchs précédents
-            faf = prec_faf.iloc[j]                      # on stocke les informations de chaque itération du match précédant dans la variable faf
-            # on commence par itérer à travers le cas où l'équipe à évaluer (Domicile) a joué un face-à-face également à domicile
-            if faf['DomicileCode'] == equipe_domicile: 
-                # on augmente la variable victoire_domicile dans l'éventualité où l'équipe à évaluer a gagné
-                if faf['Cible'] == 1:
-                    victoires_domicile += 1
-                # on augmente la variable victoire_extérieur dans l'éventualité où l'équipe à évaluer a perdu
-                elif faf['Cible'] == 0:
-                    victoires_exterieur += 1
-            # on répète la même procédure dans le cas où l'équipe à évaluer a joué un face-à-face à l'extérieur
-            elif faf['DomicileCode'] == equipe_exterieur: 
-                if faf['Cible'] == 1:
-                    victoires_exterieur += 1
-                elif faf['Cible'] == 0:
-                    victoires_domicile += 1
+        match_actuel = matchs.iloc[i]                    
+        equipe_domicile = match_actuel['DomicileCode']   
+        equipe_exterieur = match_actuel['ExterieurCode'] 
+        date_match = match_actuel['Date']                
+
+        matchs_prec = matchs[matchs['Date'] < date_match]
         
-        # on met à jour les colonnes récoltant le total des victoires et matchs nuls en face-à-face
-        matchs.at[i, 'FAF_VictoiresDomicile'] = victoires_domicile
-        matchs.at[i, 'FAF_VictoiresExterieur'] = victoires_exterieur
-    
+        cas1 = matchs_prec[(matchs_prec['DomicileCode'] == equipe_domicile) & (matchs_prec['ExterieurCode'] == equipe_exterieur)]
+        cas2 = matchs_prec[(matchs_prec['DomicileCode'] == equipe_exterieur) & (matchs_prec['ExterieurCode'] == equipe_domicile)]
+        
+        victoires_domicile_a_dom = sum(cas1['Cible'] == 1)  
+        victoires_exterieur_a_ext = sum(cas1['Cible'] == 0) 
+        nul_dom_a_dom = sum(cas1['Cible'] == 2)             
+        
+        victoires_exterieur_a_dom = sum(cas2['Cible'] == 1) 
+        victoires_domicile_a_ext = sum(cas2['Cible'] == 0)  
+        nul_dom_a_ext = sum(cas1['Cible'] == 2)             
+
+        
+        matchs.at[i, 'FAF_VictoiresDomicile_Dom'] = victoires_domicile_a_dom        
+        matchs.at[i, 'FAF_VictoiresDomicile_Ext'] = victoires_domicile_a_ext        
+        matchs.at[i, 'FAF_VictoiresExterieur_Dom'] = victoires_exterieur_a_dom      
+        matchs.at[i, 'FAF_VictoiresExterieur_Ext'] = victoires_exterieur_a_ext      
+        matchs.at[i, 'FAF_Nul_Domicile_Dom'] = nul_dom_a_dom                        
+        matchs.at[i, 'FAF_Nul_Domicile_Ext'] = nul_dom_a_ext                        
+        
+        matchs['FAF_Diff'] = matchs['FAF_VictoiresDomicile_Dom'] - matchs['FAF_VictoiresExterieur_Ext']
+        matchs['FAF_DiffGlobal'] = (matchs['FAF_VictoiresDomicile_Dom'] - matchs['FAF_VictoiresExterieur_Ext']) + (matchs['FAF_VictoiresDomicile_Ext'] - matchs['FAF_VictoiresExterieur_Dom'])
+        matchs['FAF_DiffNul'] = matchs['FAF_Nul_Domicile_Dom'] - matchs['FAF_Nul_Domicile_Ext']
     return matchs
 
-best_params = {
- 'subsample': 0.7,
- 'scale_pos_weight': 1.225,
- 'reg_lambda': 10.0,
- 'reg_alpha': 0.5,
- 'n_estimators': 1500,
- 'min_child_weight': 7,
- 'max_depth': 4,
- 'learning_rate': 0.01,
- 'gamma': 0.1,
- 'colsample_bytree': 0.8,
- 'colsample_bylevel': 0.8
- }
+with open('xgb_model.pkl', 'rb') as file:
+    xgb_model = pickle.load(file)
 
-xgb_model = xgb.XGBClassifier()
-xgb_model.load_model('xgb_model.json')
+def predire_matchs(domicile, exterieur, date):
 
-def predire_matchs(domicile, exterieur, match_date):
-
-    predire_df = matchs
+    predire_df = matchs.copy()
 
     features = [
-        'DomicileCode', 'ExterieurCode', 'DomicileForme',
-        'ExterieurForme', 'DiffForme', 'FAF_VictoiresDomicile', 
-        'FAF_VictoiresExterieur', 'SemaineSaison', 'Mois', 'AnneeSaison',
-        'DomicileAvgButsMarques_Home', 'DomicileAvgButsEncaisses_Home',
-        'DomicileAvgButsMarques_Away', 'DomicileAvgButsEncaisses_Away',
-        'ExterieurAvgButsMarques_Home', 'ExterieurAvgButsEncaisses_Home',
-        'ExterieurAvgButsMarques_Away', 'ExterieurAvgButsEncaisses_Away',
-        'DiffButsDomicile', 'DiffButsExterieur', 'DiffButsGlobal'
-        #'DiffButs',
+        'DomicileCode', 'ExterieurCode', 'DomicileForme', 'ExterieurForme', 
+        'DiffForme', 'FAF_VictoiresDomicile_Dom', 'FAF_Diff', 'FAF_DiffGlobal', 
+        'FAF_VictoiresExterieur_Dom', 'FAF_VictoiresDomicile_Ext', 
+        'FAF_VictoiresExterieur_Ext', 'SemaineSaison', 'Mois', 'AnneeSaison',
+        'DomicileAvgButsMarques_Home', 'DomicileAvgButsEncaisses_Home', 
+        'DomicileAvgButsMarques_Away', 'DomicileAvgButsEncaisses_Away', 
+        'ExterieurAvgButsMarques_Home', 'ExterieurAvgButsEncaisses_Home', 
+        'ExterieurAvgButsMarques_Away', 'ExterieurAvgButsEncaisses_Away', 
+        'DiffButsDomicile', 'DiffButsExterieur', 'DiffButs', 'DiffButsGlobal',
+        'FAF_Nul_Domicile_Dom', 'FAF_Nul_Domicile_Ext', 'FAF_DiffNul' 
     ]
-    
-    new_row = {col: 0 for col in features if col in predire_df.columns}
-    date = pd.to_datetime(match_date)
+
+    new_row = {}
+    for col in features:
+        if col in predire_df.columns:
+            new_row[col] = 0
+
+    date = pd.to_datetime(date)
     new_row['Date'] = date
     new_row['DomicileCode'] = code_equipe.get(domicile)
     new_row['ExterieurCode'] = code_equipe.get(exterieur)
     new_row['Mois'] = date.month
-    
     if date > pd.Timestamp(date.year, month=8, day=5):
         new_row['AnneeSaison'] = date.year - 1
     else:
         new_row['AnneeSaison'] = date.year
-    reference_date = pd.Timestamp(year=date.year, month=8, day=5)
-    new_row['SemaineSaison'] = ((date - reference_date).days // 7) + 1
+    new_row['SemaineSaison'] = (matchs.apply(lambda x: (x['Date'] - pd.Timestamp(year=x['AnneeSaison'], month=8, day=5)).days, axis=1)) // 7 + 1
     
     matchs.loc[len(predire_df)] = new_row
 
-    predire_df = stats_equipe(predire_df)
-    predire_df = faf_equipes(predire_df)
-
-    X_pred = predire_df.sort_values('Date', ascending=False).head(1)[features]
+    stats_df = stats_equipe(predire_df)
+    predire_df = faf_equipes(stats_df)
+    X_pred = predire_df.iloc[-1:][features]
     y_pred = xgb_model.predict(X_pred)
+    y_pred_prob = xgb_model.predict_proba(X_pred)
 
-    return y_pred
+    return y_pred, y_pred_prob
